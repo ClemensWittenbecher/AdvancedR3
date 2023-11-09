b00c9db03df19edeca06c25958a38617bc833d98
Clemens Wittenbecher <clemens.wittenbecher@chalmers.se>
2023-11-09 14:59:13 +0100

    remove blanks

--- R/functions.R
+++ R/functions.R
@@ -10,8 +10,6 @@ descriptive_stats <- function(data) {
         dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
         dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~round(.x, digits = 1)))
 }
-
-
 #' Metabolite distribution plot function
 #'
 #' @param data Lipidomics dataset.
