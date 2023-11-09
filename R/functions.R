1a949b648df6ca5d661f58b6488c524c19840b4c
Clemens Wittenbecher <clemens.wittenbecher@chalmers.se>
2023-11-09 14:58:16 +0100

    add plotting function

--- R/functions.R
+++ R/functions.R
@@ -10,3 +10,18 @@ descriptive_stats <- function(data) {
         dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
         dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~round(.x, digits = 1)))
 }
+
+
+#' Metabolite distribution plot function
+#'
+#' @param data Lipidomics dataset.
+#'
+#' @return a facet plot with histograms
+#'
+plot_distributions <-
+    function(data){
+        data %>%
+            ggplot2::ggplot(aes(x = value)) +
+            ggplot2::geom_histogram() +
+            ggplot2::facet_wrap(vars(metabolite), scales = "free")
+    }
